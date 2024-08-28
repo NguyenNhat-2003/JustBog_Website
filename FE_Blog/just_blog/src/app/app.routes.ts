02016f8c1f9e0ebab1ecbdcf47cf5938fa7d3b45
Anh Tuan <thananhtuan2011@gmail.com>
2024-08-28 20:59:20 +0700

    up

--- FE_Blog/just_blog/src/app/app.routes.ts
+++ FE_Blog/just_blog/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Auth/login/login.component';
+import { HomeComponent } from './Home/home/home.component';
+import { AboutComponent } from './About/about/about.component';
+import { ContactComponent } from './Contact/contact/contact.component';
+import { PostComponent } from './Post/post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    },
+    {
+        path: 'post',
+        component: PostComponent
+    },
+];

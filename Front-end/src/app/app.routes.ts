52f34620a77036635c45196b64adf3f595d2a3e5
NDnhat2003 <nguyenducnhat_t66@hus.edu.vn>
2024-08-28 19:42:06 +0700

    update fe

--- Front-end/src/app/app.routes.ts
+++ Front-end/src/app/app.routes.ts
@@ -1,54 +1,27 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { ContactComponent } from './contact/contact.component';
-import { BlogviewComponent } from './blogview/blogview.component';
-import { PostsComponent } from './posts/posts.component';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routeConfig: Routes = [
+export const routes: Routes = [
     {
-        path: '',
-        redirectTo: 'welcome',
-        pathMatch: 'full'
-    },
-    {
-        path: 'welcome',
-        component: WelcomeComponent,
-        title: 'Welcome page'
-    },
-    {
-        path: 'home',
-        component: HomeComponent,
-        title: 'Home page'
+        path: '', 
+        redirectTo:'login', 
+        pathMatch:'full'
     },
     {
         path: 'login',
-        component: LoginComponent,
-        title: 'Login page',
-        
+        component:LoginComponent
     },
     {
-        path: 'posts',
-        component: PostsComponent,
-        title: 'Posts page'
-    },
-    {
-        path: 'blogview',
-        component: BlogviewComponent,
-        title: 'blogview page'
-    },
-    {
-        path: 'contact',
-        component: ContactComponent,
-        title: 'Contact page'
-    },
-    {
-        path: 'aboutus',
-        component: AboutusComponent,
-        title: 'Aboutus page'
+        path:'',
+        component:LayoutComponent,
+        children:[
+            {
+                path:'dashboard',
+                component:DashboardComponent
+            }
+        ]
     }
   ];
-  
-export const routes: Routes = routeConfig;
+
--- Front-end/src/app/login/login.component.ts
+++ Front-end/src/app/login/login.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor (private router: Router) {}
-  OnClickHome(): void {
-    this.router.navigateByUrl('')
+OnClickHome() {
+  this.router.navigateByUrl('')
+}
+  logibObj:any = {
+    "EmailId": "",
+    "Password": ""
+  };
+  
+  constructor (private router: Router) {
+  }
+  
+  
+  http= inject(HttpClient);
+
+  onLogin() {
+    debugger;
+    this.http.post("https://freeapi.miniprojectideas.com/api/User/Login", this.logibObj).subscribe((res:any) => {
+      if (res.result) {
+        alert("Login Sucess");
+      } else {
+        alert("Check username or password");
+      }
+    })
   }
 }
